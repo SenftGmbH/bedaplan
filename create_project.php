8e7dc0d22913a278a4546a12d80a12da15782844
Andre Nitschke <info@senft-betonbohren.de>
2013-11-15 07:39:23 +0100

    Update create_project.php

--- create_project.php
+++ create_project.php
@@ -16,6 +16,7 @@ $project_date = $_POST["project_date"];
 $project_nr = $_POST["project_nr"];
 $project_description = $_POST["project_description"];
 $project_employee = $_POST["project_employee"];
+$project_done = '0';
 
 // take a look at the data
 echo "Schauen wir was wir haben:<br><br>";
@@ -29,8 +30,12 @@ echo "<br><br>von wem:";
 echo $project_employee;
 
 // lets insert the data with a sql query
+// first query insert the data into the project table
+$bedaplan_query = "INSERT INTO project (project_date, project_nr, project_description, project_employee, project_done) VALUES ('$project_date', '$project_nr', '$project_description', '$project_employee', '$project_done')";
+$bedaplan_result = mysqli_query($db, $bedaplan_query);
 
-$bedaplan_query = "INSERT INTO project (project_date, project_nr, project_description, project_employee) VALUES ('$project_date', '$project_nr', '$project_description', '$project_employee')";
+// secound query insert the data into the project_time table so that we can work with update in the frontent and must not create a entry
+$bedaplan_query = "INSERT INTO projecttime (pt_nr, pt_employee, pt_sort) VALUES ('$project_nr', '$project_employee', '$project_date')";
 $bedaplan_result = mysqli_query($db, $bedaplan_query);
 
 
