42eabe5d3b8b866a02c076a827a1d5bd9f6d33a4
Andre Nitschke <info@senft-betonbohren.de>
2013-11-15 07:42:47 +0100

    Create update_projecttime.php

---
+++ update_projecttime.php
@@ -0,0 +1,39 @@
+<html>
+ <head>
+  <title>bedaplan</title>
+ </head>
+ <body background="background.jpg">
+ <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+<hr>
+<br>
+<?php
+// connect to the database
+$db = mysqli_connect("localhost", "user", "password", "database");
+
+// and show if there any errors
+if(!$db)
+{
+  exit("Verbindungsfehler: ".mysqli_connect_error());
+}
+
+
+// get the variables from the mainsite
+$project_nr = $_GET["current_nr"];
+$project_employee = $_GET["current_user"];
+
+$timeget = time(); 
+$uhrzeit = date("H:i",$timeget); 
+// set the project start time to the current time
+$bedaplan_query = "UPDATE projecttime SET pt_start= NOW() WHERE pt_nr = '$project_nr'"; 
+$bedaplan_result = mysqli_query($db, $bedaplan_query);
+
+echo "Projektnummer: <br>";
+echo $project_nr;
+echo "<br><br>Startzeit:<br>";
+echo $uhrzeit;
+
+?>
+<script type="text/javascript">
+     window.setTimeout("this.close()",3000);
+        </script>
+</body>
