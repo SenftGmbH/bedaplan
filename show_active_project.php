6c4331381a8e592035e33888f311501ecf69988f
Andre Nitschke <info@senft-betonbohren.de>
2013-11-19 09:58:03 +0100

    Create show_active_project.php

---
+++ show_active_project.php
@@ -0,0 +1,63 @@
+<html>
+ <head>
+  <title>bedaplan</title>
+ </head>
+ <body>
+ <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+  <p align="right"><font size="5">Admin - Laufende Projekte</font></p>
+<hr>
+<br>
+<?php
+// connect to the database
+$db = mysqli_connect("localhost", "username", "userpassword", "database");
+
+// and show if there any errors
+if(!$db)
+{
+  exit("Verbindungsfehler: ".mysqli_connect_error());
+}
+
+
+
+
+
+
+// create the quere for the worktime of the user
+$bedaplan_query = "SELECT * FROM project WHERE project_done = '1'"; 
+
+$bedaplan_result = mysqli_query($db, $bedaplan_query);
+
+
+
+
+echo "Offene Projekte:<br><br>";
+//create the table with the active projects
+echo "<table border=1>";
+while($row = $bedaplan_result->fetch_assoc()) 
+    { 
+	echo "<tr>";
+	echo "<td>";
+	echo $row["project_date"];
+	echo "</td>";
+ 	echo "<td style='width:100px'>";
+	echo $row["project_nr"];
+	echo "</td>";
+	echo "<td style='width:300px'>";
+	echo $row["project_description"];
+	echo "</td>";
+	echo "<td style='width:200px'>";
+	echo $row["project_employee"];
+	echo "</td>";
+	echo "</tr>";
+    } 
+
+echo "</table>";
+echo "<br><br>";
+echo "Totale Arbeitszeit: ";
+echo "<b>";
+echo round($total/60/60,2);
+echo "</b>";
+
+
+?>
+</html>
