1032ee71e780ee7ef2c999bf200ffe1bc480c924
Andre Nitschke <info@senft-betonbohren.de>
2013-11-22 08:43:21 +0100

    Update update_worktime.php

--- update_worktime.php
+++ update_worktime.php
@@ -8,7 +8,7 @@
 <br>
 <?php
 // connect to the database
-$db = mysqli_connect("localhost", "username", "userpassword", "database");
+$db = mysqli_connect("localhost", "user", "password", "database");
 
 // and show if there any errors
 if(!$db)
@@ -26,16 +26,33 @@ $uhrzeit = date("H:i",$timeget);
 $leerzeit = date_create('1971-01-01 0:0:0');
 $leer=  date_format($leerzeit, 'Y-m-d H:i:s');
 echo $leer;
-// look for an empty time of the employee and write the current time as endtime
-$bedaplan_query = "UPDATE worktime SET wt_stop= NOW() WHERE  wt_employee = '$current_employee' AND wt_stop='$leer'"; 
-$bedaplan_result = mysqli_query($db, $bedaplan_query);
-echo $bedaplan_query;
+
+// here we query the status of status_wt to look if the employee has logged in yet
+$bedaplan_query = "SELECT * FROM status WHERE status_employee = '$current_employee'";
+	$bedaplan_result = mysqli_query($db, $bedaplan_query);
+	$status_raw = $bedaplan_result->fetch_assoc();
+	$status_login = $status_raw["status_wt"];
 
 
-echo "Mitarbeiter: <br>";
-echo $current_employee;
-echo "<br><br>Endzeitzeit:<br>";
-echo $uhrzeit;
+if ($status_login==2)
+ {
+ // user is logged in, so show error message
+ echo "<b>Mitarbeiter hat sich bereits abgemeldet !!!</b>";
+ }
+else
+ {
+ //user is logged in so start the logout process
+ // look for an empty time of the employee and write the current time as endtime
+ $bedaplan_query = "UPDATE worktime SET wt_stop= NOW() WHERE  wt_employee = '$current_employee' AND wt_stop='$leer'"; 
+ $bedaplan_result = mysqli_query($db, $bedaplan_query);
+ // set the status of worktime = 2
+ $bedaplan_query = "UPDATE status SET status_wt = '2' WHERE  status_employee = '$current_employee'";
+ $bedaplan_result = mysqli_query($db, $bedaplan_query);
+ echo "Mitarbeiter: <br>";
+ echo $current_employee;
+ echo "<br><br>Endzeitzeit:<br>";
+ echo $uhrzeit;
+ }
 
 ?>
 <script type="text/javascript">
