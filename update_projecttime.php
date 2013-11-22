db367ae30b1c42b87f49afadf382f17729ff956a
Andre Nitschke <info@senft-betonbohren.de>
2013-11-22 08:45:43 +0100

    Update update_projecttime.php

--- update_projecttime.php
+++ update_projecttime.php
@@ -8,7 +8,7 @@
 <br>
 <?php
 // connect to the database
-$db = mysqli_connect("localhost", "username", "userpassword", "database");
+$db = mysqli_connect("localhost", "user", "password", "database");
 
 // and show if there any errors
 if(!$db)
@@ -23,18 +23,39 @@ $project_employee = $_GET["current_user"];
 
 $timeget = time(); 
 $uhrzeit = date("H:i",$timeget); 
-// set the project start time to the current time
-$bedaplan_query = "UPDATE projecttime SET pt_start= NOW() WHERE pt_nr = '$project_nr'"; 
-$bedaplan_result = mysqli_query($db, $bedaplan_query);
 
-// set the project status to working
-$bedaplan_query = "UPDATE project SET project_done='1' WHERE project_nr = '$project_nr'"; 
-$bedaplan_result = mysqli_query($db, $bedaplan_query);
+// here we query the status of status_wt to look if the employee has logged in yet
+$bedaplan_query = "SELECT * FROM status WHERE status_employee = '$project_employee'";
+	$bedaplan_result = mysqli_query($db, $bedaplan_query);
+	$status_raw = $bedaplan_result->fetch_assoc();
+	$status_login = $status_raw["status_pt"];
 
-echo "Projektnummer: <br>";
-echo $project_nr;
-echo "<br><br>Startzeit:<br>";
-echo $uhrzeit;
+
+
+
+if ($status_login==1)
+ {
+ // user is logged in, so show error message
+ echo "<b>Mitarbeiter hat sich bereits bei einem Projekt angemeldet !!!</b>";
+ }
+else
+ {
+  // set the project start time to the current time
+  $bedaplan_query = "UPDATE projecttime SET pt_start= NOW() WHERE pt_nr = '$project_nr'"; 
+  $bedaplan_result = mysqli_query($db, $bedaplan_query);
+
+  // set the project status to working
+  $bedaplan_query = "UPDATE project SET project_done='1' WHERE project_nr = '$project_nr'"; 
+  $bedaplan_result = mysqli_query($db, $bedaplan_query);
+  // set the employee status to 1
+  $bedaplan_query = "UPDATE status SET status_pt = '1' WHERE  status_employee = '$project_employee'";
+  $bedaplan_result = mysqli_query($db, $bedaplan_query);
+
+  echo "Projektnummer: <br>";
+  echo $project_nr;
+  echo "<br><br>Startzeit:<br>";
+  echo $uhrzeit;
+}
 
 ?>
 <script type="text/javascript">
