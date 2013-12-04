f445f425783cfb8e6050a0a17731395ce80ef60b
Andre Nitschke <info@senft-betonbohren.de>
2013-12-04 13:57:07 +0100

    Create admin_adduser_project.php

---
+++ admin_adduser_project.php
@@ -0,0 +1,42 @@
+<html>
+ <head>
+  <title>bedaplan Admin - Create Project</title>
+ </head>
+
+ <body>
+  <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+  <p align="right"><font size="5">Admin - Benutzer zum Projekt hinzuf&uuml;gen</font></p>
+  <hr>
+  <!-- Reciving the project data and send it to adduser_project.php -->
+
+  <form action="http://213.23.35.50/bedaplan/adduser_project.php" method="post">
+    <p>Projektnummer: <input name="project_nr" type="text" size="30" maxlength="30"></p>
+    <select name="project_employee" size="3">
+     <option>sbodner</option>
+     <option>shagen</option>
+     <option>wandronnikow</option>
+     <option>dbull</option>
+     <option>cexner</option>
+     <option>cflemmer</option>
+     <option>mgallinat</option>
+     <option>agalster</option>
+     <option>ngalster</option>
+     <option>oheinz</option>
+     <option>pkalb</option>
+     <option>wklaas</option>
+     <option>fkretke</option>
+     <option>vmiller</option>
+     <option>rnowokowski</option>
+     <option>enussbaum</option>
+     <option>rruehle</option>
+     <option>mschwarz</option>
+     <option>rlopez</option>
+
+    </select>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="submit" value=" Projekt Erstellen ">
+  </form>
+  
+
+ <body>
+</html>
