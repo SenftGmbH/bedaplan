bcf41aa9ca88d6c1fbd1fe6440d942b71b9852fe
Andre Nitschke <info@senft-betonbohren.de>
2013-12-04 14:15:04 +0100

    Update create_project.php

--- create_project.php
+++ create_project.php
@@ -30,12 +30,20 @@ echo "<br><br>von wem:";
 echo $project_employee;
 
 // lets insert the data with a sql query
-// first query insert the data into the project table
-$bedaplan_query = "INSERT INTO project (project_date, project_nr, project_description, project_employee, project_done) VALUES ('$project_date', '$project_nr', '$project_description', '$project_employee', '$project_done')";
+// first query insert the data into the project table and i set the number of users to 1 because the project is new
+$bedaplan_query = "INSERT INTO project (project_date, project_nr, project_description, project_employee, project_users, project_done) VALUES ('$project_date', '$project_nr', '$project_description', '$project_employee', '1', '$project_done')";
 $bedaplan_result = mysqli_query($db, $bedaplan_query);
 
+
+// this function i plan not to use anymore. the timetable should pe creates with each time-update. 29.11.2013 a.nitschke
 // secound query insert the data into the project_time table so that we can work with update in the frontent and must not create a entry
-$bedaplan_query = "INSERT INTO projecttime (pt_nr, pt_employee, pt_sort) VALUES ('$project_nr', '$project_employee', '$project_date')";
+// $bedaplan_query = "INSERT INTO projecttime (pt_nr, pt_employee, pt_sort) VALUES ('$project_nr', '$project_employee', '$project_date')";
+// $bedaplan_result = mysqli_query($db, $bedaplan_query);
+
+
+
+// now we add the project to the project_users so we can see how much people work on the project
+$bedaplan_query = "INSERT INTO projectuser (project_id, project_users) VALUES ('$project_nr', '0')";
 $bedaplan_result = mysqli_query($db, $bedaplan_query);
 
 
