1116fd9e0d49339eb8f71de55234c2fb9d45f876
Andre Nitschke <info@senft-betonbohren.de>
2013-12-13 08:56:26 +0100

    Update update_projecttime.php

--- update_projecttime.php
+++ update_projecttime.php
@@ -1,6 +1,7 @@
 <html>
  <head>
   <title>bedaplan</title>
+<meta name="viewport" content="width=500" />
  </head>
  <body background="background.jpg">
  <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
@@ -68,6 +69,6 @@ $bedaplan_query = "INSERT INTO projecttime (pt_nr, pt_employee, pt_start) VALUES
 
 ?>
 <script type="text/javascript">
-     window.setTimeout("this.close()",2000);
+     window.setTimeout("this.close()",4000);
         </script>
 </body>
