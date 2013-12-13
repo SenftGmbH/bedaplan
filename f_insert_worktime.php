ccc08fc23220c972dc1b20d04667ba2b09381d0b
Andre Nitschke <info@senft-betonbohren.de>
2013-12-13 08:36:06 +0100

    Create f_insert_worktime.php

---
+++ f_insert_worktime.php
@@ -0,0 +1,25 @@
+
+<?php
+
+$mitarbeiter = $_GET["current_user"];
+$current_nr = $_GET["current_nr"];
+	
+?>
+
+<html>
+ <head>
+  <title>bedaplan</title>
+  <meta name="viewport" content="width=500" />
+  <meta http-equiv="refresh" content="60;url=http://213.23.35.50/bedaplan/bedaplan1.php?mitarbeiter=<?php echo $mitarbeiter ?>">
+ </head>
+ <body background="background.jpg">
+ <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+Arbeitsbeginn f&uuml;r Mitarbeiter <?php echo $mitarbeiter ?>
+<br><br>
+<center>
+ <a href="http://213.23.35.50/bedaplan/insert_worktime.php?current_user=<?php echo $mitarbeiter ?>"  target="_blank"><img src="arbeitsbeginn.jpg" border=0></a><br><br><br>
+
+ <a href="javascript:window.close()"><img src="hauptseite.jpg" border=0></a>
+</center>  
+ </body>
+</html>
