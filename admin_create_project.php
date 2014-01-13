d74b65a2b58f87eb4b446589cf96a619a1576cdb
Andre Nitschke <info@senft-betonbohren.de>
2014-01-13 15:48:30 +0100

    Update admin_create_project.php

--- admin_create_project.php
+++ admin_create_project.php
@@ -10,10 +10,10 @@
   <!-- Reciving the project data and send it to create_project.php -->
 
   <form action="http://213.23.35.50/bedaplan/create_project.php" method="post">
-    <p>Projektdatum: <input name="project_date" type="text" size="30" maxlength="30"></p>
-    <p>Projektnummer: <input name="project_nr" type="text" size="30" maxlength="30"></p>
+    <p>Projektdatum: <input name="project_date" type="text" size="30" maxlength="30"><- z.b. 14.01.2014</p>
+    <p>Projektnummer: <input name="project_nr" type="text" size="30" maxlength="30"><- z.b. 43667</p>
     <p>Projektbeschreibung:<br>
-    <textarea name="project_description" cols="50" rows="10"></textarea>
+    <textarea name="project_description" cols="50" rows="10"></textarea><- Hier die Adresse und Beschreibung eingeben!
     </p>
     <p>Kartenlink: <input name="map" type="text" size="30" maxlength="256"></p>
 
