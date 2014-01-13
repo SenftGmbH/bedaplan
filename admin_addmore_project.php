ed7ef90fcebf52913cad5aae379189ab395b3347
Andre Nitschke <info@senft-betonbohren.de>
2014-01-13 15:46:03 +0100

    Create admin_addmore_project.php

---
+++ admin_addmore_project.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+  <title>bedaplan Admin - Create Project</title>
+ </head>
+
+ <body>
+  <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+  <p align="right"><font size="5">Admin - Projekt um einen Tag verl&auml;ngern</font></p>
+  <hr>
+  <!-- Reciving the project data and send it to adduser_project.php -->
+
+  <form action="http://213.23.35.50/bedaplan/addmore_project.php" method="post">
+    <p>Projektnummer: <input name="project_nr" type="text" size="30" maxlength="30"></p>
+    <p>Mitarbeiter:<input name="project_employee" type="text" size="30" maxlenght="30"></p>
+    <p>Altes Datum:<input name="project_date" type="text" size="30" maxlenght="30"></p>
+    <p>Weiteres Datum:<input name="project_newdate" type="text" size="30" maxlenght="30"></p>
+    </select>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="submit" value=" Projekt Verl&auml;ngern ">
+  </form>
+<p align="right">  
+<a href="javascript:window.close()"><img src="close.gif" alt="Fenster schliessen" border=0></a>
+</p>
+ <body>
+</html>
