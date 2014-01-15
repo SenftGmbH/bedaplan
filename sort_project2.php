3d82627d25cbb3c90c1510c2f08e2732d0a1c9e0
Andre Nitschke <info@senft-betonbohren.de>
2014-01-15 06:43:35 +0100

    Update sort_project2.php

--- sort_project2.php
+++ sort_project2.php
@@ -9,7 +9,7 @@
 <br>
 <?php
 // connect to the database
-$db = mysqli_connect("localhost", "username", "password", "database");
+$db = mysqli_connect("localhost", "user", "password", "database");
 
 // and show if there any errors
 if(!$db)
