53272f27ddc5ef88527d50819db4cfdfa66ea9af
Andre Nitschke <info@senft-betonbohren.de>
2014-03-03 08:31:24 +0100

    Create admin_addfile_project.php

---
+++ admin_addfile_project.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>bedaplan</title>
+ </head>
+ <body>
+ <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+  <p align="right"><font size="5">Admin - Datei einem Projekt zuordnen</font></p>
+<hr>
+<br>
+Bitte die Datei ausw&auml;hlen!
+<br>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+ <input type="file" name="myFile">
+ <br>
+ <input type="submit" value="Datei hinzuf&uuml;gen">
+</form>
+</html>
