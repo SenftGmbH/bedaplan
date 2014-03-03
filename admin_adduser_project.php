119eae30b314527b9410c0774bccc6799364ab3b
Andre Nitschke <info@senft-betonbohren.de>
2014-03-03 08:34:49 +0100

    Update admin_adduser_project.php

--- admin_adduser_project.php
+++ admin_adduser_project.php
@@ -1,50 +1,43 @@
-<?php
-// connect to the database
-$db = mysqli_connect("localhost", "dbuser", "dbpassword", "dbname");
+<html>
+ <head>
+  <title>bedaplan Admin - Create Project</title>
+ </head>
 
-// and show if there any errors
-if(!$db)
-{
-  exit("Verbindungsfehler: ".mysqli_connect_error());
-}
+ <body>
+  <img src="http://213.23.35.50/bedaplan/bedaplanlogo.jpg">
+  <p align="right"><font size="5">Admin - Benutzer zum Projekt hinzuf&uuml;gen</font></p>
+  <hr>
+  <!-- Reciving the project data and send it to adduser_project.php -->
 
+  <form action="http://213.23.35.50/bedaplan/adduser_project.php" method="post">
+    <p>Projektnummer: <input name="project_nr" type="text" size="30" maxlength="30"></p>
+    <select name="project_employee" size="3">
+     <option>sbodner</option>
+     <option>shagen</option>
+     <option>wandronnikow</option>
+     <option>dbull</option>
+     <option>cexner</option>
+     <option>cflemmer</option>
+     <option>mgallinat</option>
+     <option>agalster</option>
+     <option>ngalster</option>
+     <option>oheinz</option>
+     <option>pkalb</option>
+     <option>wklaas</option>
+     <option>fkretke</option>
+     <option>vmiller</option>
+     <option>rnowokowski</option>
+     <option>enussbaum</option>
+     <option>rruehle</option>
+     <option>mschwarz</option>
+     <option>plopez</option>
 
-// $bedaplan_query = "SELECT url FROM links";
-
-// get the information from the admin page
-$project_nr = $_POST["project_nr"];
-$project_employee = $_POST["project_employee"];
-
-
-
-
-// take a look at the data
-echo "Schauen wir was wir haben:<br><br>";
-
-echo "<br><br>Projektnummer:<br>";
-echo $project_nr;
-echo "<br><br>von wem:";
-echo $project_employee;
-// get the description and date from the database
-$bedaplan_query = "SELECT * FROM project WHERE project_nr = '$project_nr' AND project_done = '0'";
-$bedaplan_result = mysqli_query($db, $bedaplan_query);
-$row = $bedaplan_result->fetch_assoc();
-$project_description =  $row["project_description"];
-$project_date = $row["project_date"];
-echo "<br>Zur Kontrolle:<br>";
-echo $project_date;
-echo $project_description;
-
-
-// lets insert the data with a sql query
-// first query insert the data into the project table and i set the number of users to 1 because the project is new
-$bedaplan_query = "INSERT INTO project (project_date, project_nr, project_description, project_employee, project_users, project_done) VALUES ('$project_date', '$project_nr', '$project_description', '$project_employee', '1', '$project_done')";
-$bedaplan_result = mysqli_query($db, $bedaplan_query);
-
-// secound query insert the data into the project_time table so that we can work with update in the frontent and must not create a entry
-$bedaplan_query = "INSERT INTO projecttime (pt_nr, pt_employee, pt_sort) VALUES ('$project_nr', '$project_employee', '$project_date')";
-$bedaplan_result = mysqli_query($db, $bedaplan_query);
-
-
-
-?>
+    </select>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="submit" value=" Projekt Erstellen ">
+  </form>
+<p align="right">  
+<a href="javascript:window.close()"><img src="close.gif" alt="Fenster schliessen" border=0></a>
+</p>
+ <body>
+</html>
